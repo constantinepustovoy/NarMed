45671d9a9958bc776808f81c7de429530fa46e04
constantinepustovoy <pustovoylug@gmail.com>
2017-02-27 14:34:54 +0200

    sticky-menu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-// alert("hi!");
+// console.log("hi!");
+
+$(function(){
+
+  $(window).scroll(function() {
+
+    if($(this).scrollTop() >= 430) {
+      $('.navigation-menu').addClass('sticky-menu');
+      $('.nav-inner-cover').addClass('nav-menu'); 
+
+    }
+    else{
+      $('.navigation-menu').removeClass('sticky-menu');
+      $('.nav-inner-cover').removeClass('nav-menu'); 
+    }
+
+  });
+
+});
